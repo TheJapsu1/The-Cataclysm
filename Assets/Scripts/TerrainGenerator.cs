62563455667e28119d314a16ae245f8246d1b2aa
japsu <japsu.honkasalo@gmail.com>
2021-09-09 20:37:19 +0300

    to 09.09.2021 20.37.17,47: procgen fixes and additions

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -7,30 +7,42 @@ using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    private void Start()
+    public GTerrainData terrainDataTemplate;
+    //public GTerrainGeneratedData terrainGeneratedDataTemplate;
+
+    [SerializeField] private float noiseScale;
+    [SerializeField] private float noiseAmplitude;
+
+    private void Update()
     {
-        GenerateTerrains();
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GStylizedTerrain t = CreateTerrain(terrainDataTemplate);
+            
+            Debug.Log(t.TerrainData.GeometryData.name);
+            
+            GenerateTerrain(t);
+        }
     }
 
-    private void GenerateTerrains()
+    private void GenerateTerrain(GStylizedTerrain t)
     {
-        using IEnumerator<GStylizedTerrain> terrains = GStylizedTerrain.ActiveTerrains.GetEnumerator();
-        while (terrains.MoveNext())
-        {
-            GStylizedTerrain t = terrains.Current;
+        Color[] oldHeightMapData = t.TerrainData.Geometry.HeightMap.GetPixels();
 
-            if (t is null) continue;
-            
-            //t.ConnectNeighbor();
+        // Set the pixels
+        t.TerrainData.Geometry.HeightMap.SetPixels(GenerateHeightmap(t));
+        t.TerrainData.Geometry.HeightMap.Apply();
+        
+        Color[] newHeightMapData = t.TerrainData.Geometry.HeightMap.GetPixels();
 
-            // Set the pixels
-            t.TerrainData.Geometry.HeightMap.SetPixels(GenerateHeightmap(t));
-            t.TerrainData.Geometry.HeightMap.Apply();
-
-            // Set the terrain to dirty, to regenerate.
-            t.TerrainData.Geometry.SetRegionDirty(GCommon.UnitRect);
-            t.TerrainData.SetDirty(GTerrainData.DirtyFlags.Geometry);
-        }
+        // Set the terrain to dirty, to regenerate.
+        IEnumerable<Rect> dirtyRects = GCommon.CompareTerrainTexture(5, oldHeightMapData, newHeightMapData);
+        
+        t.TerrainData.Geometry.SetRegionDirty(dirtyRects);
+        t.TerrainData.SetDirty(GTerrainData.DirtyFlags.GeometryTimeSliced);
+        
+        //Clean up
+        t.TerrainData.Geometry.ClearDirtyRegions();
     }
 
     private Color[] GenerateHeightmap(GStylizedTerrain terrain)
@@ -44,8 +56,11 @@ public class TerrainGenerator : MonoBehaviour
             for (int y = 0; y < terrain.TerrainData.Geometry.HeightMap.height; y++)
             {
                 int pixelIndex = y * resolution + x;
+                
                 Color c = data[pixelIndex];
-                c.r = GetElevationAt(x, y);
+                
+                c.r = GetElevationAt(x + terrain.transform.position.x, y + terrain.transform.position.z);
+                
                 data[pixelIndex] = c;
             }
         }
@@ -53,8 +68,31 @@ public class TerrainGenerator : MonoBehaviour
         return data;
     }
 
-    private float GetElevationAt(int x, int y)
+    private float GetElevationAt(float x, float y)
     {
-        return (Mathf.PerlinNoise(x, y) * 50) - 200;
+        float elevation = Mathf.PerlinNoise(x / noiseScale, y / noiseScale) * noiseAmplitude;
+        
+        return elevation;
+    }
+    
+    private GStylizedTerrain CreateTerrain(GTerrainData data)
+    {
+        GameObject g = new GameObject("Stylized Terrain")
+        {
+            transform =
+            {
+                position = transform.position,
+                localRotation = Quaternion.identity,
+                localScale = Vector3.one
+            }
+        };
+
+        GStylizedTerrain terrain = g.AddComponent<GStylizedTerrain>();
+        terrain.GroupId = 0;
+        //terrain.TerrainData = Instantiate(data);
+        terrain.TerrainData = data;
+        //terrain.TerrainData.GeometryData = Instantiate(terrainGeneratedDataTemplate);
+        terrain.Refresh();
+        return terrain;
     }
 }
